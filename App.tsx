a66935530903e5504ba8eb0dce1db7b7b77360af
Sinn <psribeirodemiranda@gmail.com>
2025-08-24 18:22:41 -0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import { View,Text } from "react-native";
+
+export default function App(){
+  return(
+    <View style={{justifyContent:'center',alignItems:'center'}}>
+      <Text style = {{justifyContent:'center'}}>Aula 1 da nossa turma</Text>
+    </View>
+  )
+}
