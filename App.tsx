f1ec17379d998bbefd95228bfa634b791f15f421
Sinn <psribeirodemiranda@gmail.com>
2025-09-03 09:16:14 -0300

    Delete App.tsx

--- App.tsx
+++
@@ -1,9 +0,0 @@
-import { View,Text } from "react-native";
-
-export default function App(){
-  return(
-    <View style={{justifyContent:'center',alignItems:'center'}}>
-      <Text style = {{justifyContent:'center'}}>Aula 1 da nossa turma</Text>
-    </View>
-  )
-}
